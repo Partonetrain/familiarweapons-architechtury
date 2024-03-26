c9301e4bc304001a67cf6fcb639821f8c62dc621
Partonetrain <Partonetrain@gmail.com>
2024-03-25 20:01:02 -0500

    Add light arrow dispenser behavior

--- common/src/main/java/info/partonetrain/familiarweapons/registry/FWDispenserBehavior.java
+++ common/src/main/java/info/partonetrain/familiarweapons/registry/FWDispenserBehavior.java
@@ -1,17 +1,24 @@
 package info.partonetrain.familiarweapons.registry;
 
+import info.partonetrain.familiarweapons.entity.LightArrowEntity;
 import info.partonetrain.familiarweapons.item.ElegantReaperScytheItem;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.BlockSource;
 import net.minecraft.core.Direction;
+import net.minecraft.core.Position;
+import net.minecraft.core.dispenser.AbstractProjectileDispenseBehavior;
 import net.minecraft.core.dispenser.OptionalDispenseItemBehavior;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.boss.wither.WitherBoss;
+import net.minecraft.world.entity.projectile.AbstractArrow;
+import net.minecraft.world.entity.projectile.Arrow;
+import net.minecraft.world.entity.projectile.Projectile;
 import net.minecraft.world.entity.projectile.WitherSkull;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.DispenserBlock;
 import org.jetbrains.annotations.NotNull;
 
@@ -39,6 +46,14 @@ public class FWDispenserBehavior {
                     this.setSuccess(true);
                 }
                 return stack;
+            }
+        });
+        DispenserBlock.registerBehavior(FWItems.LIGHT_ARROW.get(), new AbstractProjectileDispenseBehavior() {
+            @Override
+            protected Projectile getProjectile(Level level, Position position, ItemStack stack) {
+                LightArrowEntity arrow = new LightArrowEntity(level, position.x(), position.y(), position.z());
+                arrow.pickup = AbstractArrow.Pickup.ALLOWED;
+                return arrow;
             }
         });
     }
